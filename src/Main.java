e6b4e6ae7aa7de1004b49666333d3d4246dc0e2f
eddvarg1 <eddvarg1@yahoo.com>
2017-09-10 23:51:45 -0500

    figure out what to split on

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,90 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       // System.out.println("Hello World!");
+        int numOfZeros=0, numOfOnes=0;
         System.out.println("Params?: " + args[0]);
-
         ArrayList<ArrayList<String>> decisionData = fileToArrayList(args[0]);
 
         //Print Array
-        for(int x=0; x<decisionData.size(); x++) {
-            for(int y=0; y<decisionData.get(x).size(); y++) {
-                System.out.print(decisionData.get(x).get(y));
+        for(int x=1; x<decisionData.get(decisionData.size()-1).size(); x++) {
+            if(decisionData.get(decisionData.size()-1).get(x).equals("1"))
+            {
+                numOfOnes++;
             }
-            System.out.println();
+            else
+                numOfZeros++;
         }
+        ArrayList<String> classList=decisionData.get(decisionData.size()-1);
+        decisionData.remove(decisionData.size()-1);
+        System.out.println(decideSplit(decisionData,classList,CalcEntropy((double)numOfOnes,(double)numOfZeros)));
+
     }
 
+    private static int decideSplit ( ArrayList<ArrayList<String>> check, ArrayList<String> test, double entropy)
+    {
+
+        int[] whenTestOne=new int[(int)Math.pow(check.size(),2)];
+        int[] whenTestZero=new int[(int)Math.pow(check.size(),2)];
+        int split=0;
+        double min=1;
+        double [] InfoGain=new double[check.size()];
+        double entropyfractionL,entropyfractionR;
+        for(int j=0; j<whenTestOne.length;j++) {
+        whenTestOne[j]=0;
+        whenTestZero[j]=0;
+        }
+
+        for(int x=0; x<check.size();x++)
+        {
+            for(int y=1; y<test.size();y++)
+            {
+
+                if(check.get(x).get(y).equals("1"))
+                {
+                    if(test.get(y).equals("1"))
+                        whenTestOne[(((x+1)*2)-1)]++;
+                    else
+                        whenTestZero[(((x+1)*2)-1)]++;
+                }
+                else
+                if(check.get(x).get(y).equals("0"))
+                {
+                    if(test.get(y).equals("1"))
+                        whenTestOne[(((x+1)*2)-2)]++;
+                    else
+                        whenTestZero[(((x+1)*2)-2)]++;
+                }
+
+            }
+
+            entropyfractionL=(double)(whenTestOne[(((x+1)*2)-1)]+whenTestOne[(((x+1)*2)-2)])/(whenTestOne[(((x+1)*2)-1)]+whenTestOne[(((x+1)*2)-2)]
+                    +whenTestZero[(((x+1)*2)-1)]+whenTestZero[(((x+1)*2)-2)]);
+            entropyfractionR=(double)(whenTestZero[(((x+1)*2)-1)]+whenTestZero[(((x+1)*2)-2)])/(whenTestOne[(((x+1)*2)-1)]+whenTestOne[(((x+1)*2)-2)]
+                    +whenTestZero[(((x+1)*2)-1)]+whenTestZero[(((x+1)*2)-2)]);
+
+           InfoGain[x]= -1*(entropy-((CalcEntropy(whenTestOne[(((x+1)*2)-1)],whenTestOne[(((x+1)*2)-2)]))*entropyfractionL
+                   +(CalcEntropy(whenTestZero[(((x+1)*2)-1)],whenTestZero[(((x+1)*2)-2)]))*entropyfractionR));
+        }
+       for(int z=0;z< InfoGain.length ;z++)
+        {
+            if(min>InfoGain[z]) {
+            min=InfoGain[z];
+            split=z;
+            }
+        }
+       return (int)split;
+
+    }
+    private static double CalcEntropy (double first, double second) {
+        if(second==0||first==0)
+            return 0;
+        return ((((-1*first)/(first+second))*(Math.log10(first/(first+second))/Math.log10(2)))+
+                ((((-1*second)/(first+second))*(Math.log10(second/(first+second))/Math.log10(2)))));
+    }
     private static ArrayList<ArrayList<String>> fileToArrayList(String fileName) {
-        File testData = new File("../project1/data/" + fileName);
-        System.out.println(testData.getAbsolutePath());
+        File testData = new File("D:\\Machine learning\\Decision-Tree\\data/" + fileName);
+        //System.out.println(testData.getAbsolutePath());
 
         try {
             Scanner sc = new Scanner(testData);
@@ -29,7 +96,7 @@ public class Main {
             int numberOfAttributes = 0;
             Scanner sc1 = new Scanner(headerLine).useDelimiter("\\s");
             while(sc1.hasNext()){
-                System.out.println(sc1.next());
+               sc1.next();
                 numberOfAttributes++;
             }
 
